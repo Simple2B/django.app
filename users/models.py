7980b6b73afbe77713ae88ef8b1b4b11697373e8
EvgeniiShcherbina <eugene@inkit.com>
2022-09-15 18:11:05 +0300

    custom user model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    username = models.CharField(max_length=60, unique=True, null=False)
+    email = models.CharField(max_length=255, unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=255, null=False)
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
