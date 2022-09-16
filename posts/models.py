95a2c2b3064982b9418817880af1037ec89af239
EvgeniiShcherbina <eugene@inkit.com>
2022-09-16 17:18:08 +0300

    post model

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=64, null=False)
+    content = models.CharField(max_length=512, null=False)
+    published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        'users.CustomUser', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"<{self.id}: {self.title} at {self.created_at}>"
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = "posts"
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,15 +6,15 @@ from posts.models import Post
 
 @pytest.fixture
 def create_user(db) -> CustomUser:
-    return CustomUser.objects.create(username='test', email='test@mail.com', password='11111111')
+    return CustomUser.objects.create(username='test', email='test@mail.com', password='11111111bbb')
 
 
 @pytest.fixture
 def create_user_form(db) -> UserCreateForm:
-    return UserCreateForm(data={'username': 'test', 'email': 'test@mail.com', 'password': '11111111'})
+    return UserCreateForm(data={'username': 'test', 'email': 'test@mail.com', 'password1': '11111111bbb', 'password2': '11111111bbb'})
 
 
 @pytest.fixture
 def create_posts(create_user) -> None:
     [Post.objects.create(title=f'title {i}', content=f'test content {i}',
-                         published=True, user=create_user.pk) for i in range(3)]
+                         published=True, user=create_user) for i in range(3)]
