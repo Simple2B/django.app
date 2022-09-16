6664aa2ffae73dfc4b3c724cf697dad0b88c9bfa
EvgeniiShcherbina <eugene@inkit.com>
2022-09-16 11:32:35 +0300

    user model - remove comments

--- users/models.py
+++ users/models.py
@@ -1,12 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    # username = models.CharField(max_length=60, unique=True, null=False)
-    # email = models.CharField(max_length=255, unique=True, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    # password = models.CharField(max_length=255, null=False)
 
     class Meta:
         verbose_name = "user"
