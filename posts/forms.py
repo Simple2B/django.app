ded67f5c4551457dae79dd2140bebeeb962bbdf5
EvgeniiShcherbina <eugene@inkit.com>
2022-09-19 13:14:00 +0300

    post create form error fix

--- posts/forms.py
+++ posts/forms.py
@@ -5,8 +5,4 @@ from .models import Post
 class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'published', 'user']
-
-        widgets = {
-            'user': forms.HiddenInput()
-        }
+        fields = ['title', 'content', 'published']
--- posts/views.py
+++ posts/views.py
@@ -17,11 +17,15 @@ class CreatePost(CreateView):
         return redirect('posts_list')
 
     def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
+        """
         form = self.get_form()
-
         if form.is_valid():
             form = form.save(commit=False)
-            form.user = CustomUser.objects.get(pk=self.request.user.pk)
+            form.user = CustomUser.objects.get(
+                pk=self.request.user.pk)
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
